6f9f1eac770a3aa7b3fd52991f36d782993baa92
Ty Conway <wtyconway@gmail.com>
2019-11-04 13:30:25 -0600

    Accept command line arguments and provide basic text feedback

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello.\n";
+int main(int argc, char** argv) {
+    cout << endl;
+
+    if (argc == 1) {
+        cout << "Program expected a file input:\n"
+             << "./a.out <filename.txt>";
+    } else if (argc == 2) {
+        cout << "Parsing " << argv[1] << " and sorting numbers:\n";
+    } else {
+        cout << "Too many inputs. Please use only one.\n";
+    }
+
+    cout << endl;
 }
