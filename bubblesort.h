cb89cf41a8d4f599682809d3dbd4894ce4f7d5f3
Ty Conway <wtyconway@gmail.com>
2019-11-05 13:47:25 -0600

    Modify bubblesort to stop at sorted index
    Previously checked all elements, even already sorted.
    Update loop to only go for number of unsorted elements,
    decreasing by one each time per bubblesort.

--- bubblesort.h
+++ bubblesort.h
@@ -9,15 +9,17 @@ void swap(int left, int right, int* arr) {
 }
 
 int* bubbleSort(int count, int* numbers) {
+    int unsorted = count - 1;
     bool swapped = false;
     do {
         swapped = false;
-        for (int i = 0; i < count - 1; i++) {
+        for (int i = 0; i < unsorted; i++) {
             if (numbers[i] > numbers[i + 1]) {
                 swap(i, i + 1, numbers);
                 swapped = true;
             }
         }
+        unsorted--;
     } while (swapped);
     return numbers;
 }
